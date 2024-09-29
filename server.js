f925f7d00dca77db8dca11da7ba399ee6a932457
morrisaiden <mwaimorris1997@gmail.com>
2024-09-29 15:39:11 +0300

    server file

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Server.js
-const express = require('express');
-const routes = require('./routes') // Import routes
+import express from 'express';
+import mainRoute from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Use the routes from routes/index.js
-app.use('/', routes);
+app.use(express.json());
+app.use(mainRoute);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port:${PORT}`);
 });
 
-module.exports = app;
+export default app;
