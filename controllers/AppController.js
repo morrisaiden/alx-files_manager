61c06f0ed0e79e37f8be3928830e94756a54d92d
morrisaiden <mwaimorris1997@gmail.com>
2024-09-29 15:41:47 +0300

    AppController file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,25 @@
 // controllers/AppController.js
-const { DBClient } = require('../utils/db');
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-    static async getStatus(req, res) {
-        const redisAlive = redisClient.isAlive();
-        const dbAlive = dbClient.isAlive();
+  static async getStatus(req, res) {
+    const isRedisAlive = redisClient.isAlive();
+    const isDbAlive = dbClient.isAlive();
 
-        res.status(200).json({ redis: redisAlive, db: dbAlive });
+    res.status(200).json({ redis: isRedisAlive, db: isDbAlive });
+  }
+
+  static async getStats(req, res) {
+    try {
+      const userCount = await dbClient.nbUsers();
+      const fileCount = await dbClient.nbFiles();
+
+      res.status(200).json({ users: userCount, files: fileCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Unable to fetch statistics' });
     }
-
-    static async getStats(req, res) {
-        try {
-            const usersCount = await dbClient.nbUsers();
-            const filesCount = await dbClient.nbFiles();
-
-            res.status(200).json({ users: usersCount, files: filesCount });
-        } catch (error) {
-            res.status(500).json({error: 'Could not fetch stats'});
-        }
-    }
+  }
 }
 
-module.exports = AppController;
+export default AppController;
