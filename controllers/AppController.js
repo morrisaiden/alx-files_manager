3bb1ae927043ba2ce4b5a4c01d96fbcd967e5070
morrisaiden <mwaimorris1997@gmail.com>
2024-09-30 12:13:29 +0300

    AppController file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,21 @@
-// controllers/AppController.js
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
-
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    res.status(200).json({
+      redis: redisStatus,
+      db: dbStatus,
+    });
   }
 
   static async getStats(req, res) {
-    try {
-      const usersCount = await dbClient.nbUsers();
-      const filesCount = await dbClient.nbFiles();
-
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Unable to fetch stats' });
-    }
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
 
