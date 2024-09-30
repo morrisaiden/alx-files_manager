01f702ec5b296484401d22c3f59fee06314203e3
morrisaiden <mwaimorris1997@gmail.com>
2024-09-30 12:34:21 +0300

    redis file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import routes from './routes';
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
