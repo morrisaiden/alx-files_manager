c57a07599b0404009b1bcd6f0f162422aa4f0925
morrisaiden <mwaimorris1997@gmail.com>
2024-09-30 11:59:14 +0300

    server file

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// server.js
 import express from 'express';
-import routes from './routes/index';
+import routes from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load all routes from routes/index.js
-app.use('/', routes);
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
