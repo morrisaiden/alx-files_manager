4a7113b25d67263f3deb60eaf707af629192b190
morrisaiden <mwaimorris1997@gmail.com>
2024-09-30 12:14:56 +0300

    index file

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-// routes/index.js
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 
 const router = Router();
 
-// GET /status => AppController.getStatus
 router.get('/status', AppController.getStatus);
-
-// GET /stats => AppController.getStats
 router.get('/stats', AppController.getStats);
 
 export default router;
